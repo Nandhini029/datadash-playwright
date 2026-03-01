b395025220fab690b0b355978aa40c45223187f0
Nandhini029 <23f1000537@ds.study.iitm.ac.in>
2026-03-01 13:49:20 +0530

    Update seed values in scrape.js

--- scrape.js
+++ scrape.js
@@ -1,6 +1,7 @@
 const { chromium } = require('playwright');
 
-const seeds = [56, 57, 58, 59, 60, 61, 62, 63, 64, 65];
+const seeds = [75, 76, 77, 78, 79, 80, 81, 82, 83, 84];
+
 
 (async () => {
   const browser = await chromium.launch();
